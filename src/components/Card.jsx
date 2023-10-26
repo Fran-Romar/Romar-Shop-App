bd01af19a6c054bfa56f3211b196b67a9096a832
Fran-Romar <franromar2002@mail.com>
2023-10-26 02:03:40 +0200

    CSS Update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 
+import Rating from "@mui/material/Rating";
+
 import "../css/Card.css";
+import { Col } from "react-bootstrap";
 
 export default function Card({
   title,
   image,
-  description,
   price,
+  rating,
   handleAgregar,
   handleQuitar,
 }) {
@@ -23,26 +26,44 @@ export default function Card({
   };
 
   return (
-    <div className="tarjeta">
-      <img src={image} alt={title} className="tarjeta-imagen" />
-      <div className="tarjeta-contenido">
-        <h3 className="tarjeta-titulo">{title}</h3>
-        <p className="tarjeta-descripcion">{description}</p>
-        <p className="tarjeta-precio">{price}</p>
+    <Col md="3" className="product-col">
+      <div className="product-card">
+        <img src={image} alt={title} className="card-image" />
+        <div className="card-content">
+          <h3 title={title} className="card-title">
+            {title}
+          </h3>
+          <div className="card-rating">
+            <Rating
+              name="half-rating-read"
+              defaultValue={rating.rate}
+              precision={0.5}
+              readOnly
+            />
+            <p className="card-count">+{rating.count} vendidos</p>
+          </div>
+          <p className="card-price">
+            <b>{price}</b>
+          </p>
+        </div>
+        {added ? (
+          <button
+            type="button"
+            className="boton-quitar"
+            onClick={() => quitar()}
+          >
+            <i className="fa fa-check"></i>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="boton-agregar"
+            onClick={() => agregar()}
+          >
+            Agregar al carrito
+          </button>
+        )}
       </div>
-      {added ? (
-        <button type="button" className="boton-quitar" onClick={() => quitar()}>
-          Quitar del carrito
-        </button>
-      ) : (
-        <button
-          type="button"
-          className="boton-agregar"
-          onClick={() => agregar()}
-        >
-          Agregar al carrito
-        </button>
-      )}
-    </div>
+    </Col>
   );
 }
--- src/routes/ComprasPage.jsx
+++ src/routes/ComprasPage.jsx
@@ -4,6 +4,8 @@ import Card from "../components/Card";
 import { ProductoContext } from "../context/ProductoContext";
 import { CarritoContext } from "../context/CarritoContext";
 
+import { Row } from "react-bootstrap";
+
 export default function ComprasPage() {
   const { products } = useContext(ProductoContext);
   const { agregarCompra, eliminarCompra } = useContext(CarritoContext);
@@ -19,17 +21,21 @@ export default function ComprasPage() {
     <>
       <h1 className="text-center">Compras</h1>
       <hr />
-      {products.map((product) => (
-        <Card
-          key={product.id}
-          title={product.title}
-          image={product.image}
-          description={product.description}
-          price={product.price}
-          handleAgregar={() => handleAgregar(product)}
-          handleQuitar={() => handleQuitar(product.id)}
-        ></Card>
-      ))}
+      <div className="cards-container">
+        <Row>
+          {products.map((product) => (
+            <Card
+              key={product.id}
+              title={product.title}
+              image={product.image}
+              price={product.price}
+              rating={product.rating}
+              handleAgregar={() => handleAgregar(product)}
+              handleQuitar={() => handleQuitar(product.id)}
+            ></Card>
+          ))}
+        </Row>
+      </div>
     </>
   );
 }
