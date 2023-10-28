bcd2d2f0dbeefd601c9ed7c0c0d8e397059ec390
Fran-Romar <franromar2002@mail.com>
2023-10-28 23:02:09 +0200

    Update

--- src/routes/CarritoPage.jsx
+++ src/routes/CarritoPage.jsx
@@ -73,7 +73,7 @@ export default function CarritoPage() {
                                 className="button-delete"
                                 onClick={() => eliminarCompra(item.id)}
                               >
-                                Eliminar
+                                Delete
                               </button>
                             </div>
                           </div>
@@ -109,7 +109,7 @@ export default function CarritoPage() {
                       disabled={listaCompras < 1}
                       onClick={() => alert("Comprado!")}
                     >
-                      Comprar
+                      Buy
                     </button>
                   </div>
                 </div>
@@ -117,66 +117,6 @@ export default function CarritoPage() {
             </Row>
           </div>
         </div>
-        // <>
-        //   <table className="table">
-        //     <thead>
-        //       <tr>
-        //         <th scope="col">Nombre</th>
-        //         <th scope="col">Precio</th>
-        //         <th scope="col">Cantidad</th>
-        //         <th scope="col">Eliminar</th>
-        //       </tr>
-        //     </thead>
-        //     <tbody>
-        //       {listaCompras.map((item) => (
-        //         <tr key={item.id}>
-        //           <th scope="row">{item.title}</th>
-        //           <th>{item.price}</th>
-        //           <th>
-        // <button
-        //   type="button"
-        //   className="btn btn-ouline-warning"
-        //   onClick={() => disminuirCantidad(item.id)}
-        // >
-        //   -
-        // </button>
-        // <button className="btn btn-warning">{item.cantidad}</button>
-        // <button
-        //   type="button"
-        //   className="btn btn-ouline-warning"
-        //   onClick={() => aumentarCantidad(item.id)}
-        // >
-        //   +
-        // </button>
-        //           </th>
-        //           <th>
-        // <button
-        //   type="button"
-        //   className="btn btn-danger"
-        //   onClick={() => eliminarCompra(item.id)}
-        // >
-        //   Eliminar
-        // </button>
-        //           </th>
-        //         </tr>
-        //       ))}
-        //       <tr>
-        //         <th>
-        // <b>TOTAL: {totalPrice()}$</b>
-        //         </th>
-        //       </tr>
-        //     </tbody>
-        //   </table>
-        //   <div className="d-grid gap-2">
-        // <button
-        //   className="btn btn-warning"
-        //   disabled={listaCompras < 1}
-        //   onClick={() => alert("Comprado!")}
-        // >
-        //   Comprar
-        // </button>
-        //   </div>
-        // </>
       )}
     </>
   );
